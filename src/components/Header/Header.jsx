7a26f77937b94a69a21de016f34ee8b75fabad23
robiul0278 <robiul0278@gmail.com>
2023-03-31 14:46:34 +0600

    navbar added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,39 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <div>
-
+      <div className="navbar mb-4 border-b-2">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-3xl font-bold">
+            Code Cafe
+          </a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-5 font-bold">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <button className="btn btn-ghost btn-circle">
+          <div className="indicator">
+            <FontAwesomeIcon icon={faUser} />
+            <span className="badge badge-xs badge-primary indicator-item"></span>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
